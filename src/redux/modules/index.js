a9c7edbebe20200df7c87465df9aa37798366c0d
shilpan <shilpanbhagat@gmail.com>
2016-01-09 22:52:36 -0800

    Setting up actions for thunk middleware

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,24 +1,6 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
 
-const ingredientsQuery = (state = {}, { field }) => {
-  return dispatch => {
-    dispatch(requestFetch(field));
-
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([
-          'Water',
-          'Milk',
-          'Curd'
-        ]);
-      }, 100); // simulate server latency
-    }).then(data =>
-      (dispatch(requestRecieve(data, field))));
-  };
-};
-
 export default combineReducers({
-  ingredientsQuery: ingredientsQuery,
   routing: routeReducer
 });
