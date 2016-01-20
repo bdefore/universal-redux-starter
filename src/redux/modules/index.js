978d405d4451255eb85a06e04612c7596b182d55
Buck DeFore <bdefore@gmail.com>
2016-01-19 21:07:52 -0800

    update to 3.0.0-rc15 with redux-async-connect

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
+import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 
 export default combineReducers({
+  reduxAsyncConnect,
   routing: routeReducer
 });
